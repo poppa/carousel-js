b3f50e6327d53cf7e4747002b745dfe5d54b8d1a
Pontus Östlund <ponost@roxen.com>
2016-11-17 23:28:50 +0100

    Initial commit

---
+++ build/carousel.js
@@ -0,0 +1,104 @@
+/*
+  Author: Pontus Östlund <https://profiles.google.com/poppanator>
+
+  Permission to copy, modify, and distribute this source for any legal
+  purpose granted as long as my name is still attached to it. More
+  specifically, the GPL, LGPL and MPL licenses apply to this software.
+*/
+// jshint esversion: 6
+
+(function (window) {
+  'use strict';
+
+  const Carousel = function (el) {
+    this.config = {
+      delay: 8000,
+      transition: 'slide'
+    };
+
+    this.slider = el.getElementsByClassName('carousel-slider')[0];
+    this.items = [];
+    this.ivalId = null;
+    this.currPos = 0;
+
+    let items = this.slider.getElementsByClassName('carousel-item');
+
+    for (let i of items) {
+      this.items.push(new Carousel.Item(i));
+    }
+
+    if (el.dataset.carouselDelay) {
+      this.config.delay = parseInt(el.dataset.carouselDelay, 10);
+    }
+
+    if (el.dataset.carouselTransition) {
+      this.config.transition = el.dataset.carouselTransition;
+    }
+
+    if (this.items.length) {
+      this.items[0].load();
+    }
+
+    if (this.items.length > 1) {
+      this.items[1].load();
+    }
+
+    this.play();
+  };
+
+  Carousel.Item = function (el) {
+    this.img = el.getElementsByTagName('img')[0];
+    this.element = el;
+    this.isLoaded = false;
+    this.src = this.img.dataset.carouselSrc;
+
+    this.img.style.display = 'none';
+  };
+
+  Carousel.Item.prototype.load = function () {
+    const my = this;
+
+    this.img.setAttribute('src', this.src);
+    this.img.onload = e => {
+      // window.console.log('Image loaded');
+      my.element.style.backgroundImage = 'url(' + my.src + ')';
+      my.isLoaded = true;
+    };
+  };
+
+  Carousel.prototype.play = function () {
+    if (this.ivalId) {
+      clearInterval(this.ivalId);
+    }
+
+    const my = this;
+
+    this.ivalId = setTimeout(() => {
+      my.next();
+      my.play();
+    }, this.config.delay);
+  };
+
+  Carousel.prototype.next = function () {
+    this.currPos += 1;
+
+    if (this.currPos >= this.items.length) {
+      this.currPos = 0;
+    } else if (this.currPos + 1 < this.items.length) {
+      if (!this.items[this.currPos + 1].isLoaded) {
+        this.items[this.currPos + 1].load();
+      }
+    }
+
+    this.slider.dataset.carouselPos = this.currPos;
+  };
+
+  window.addEventListener('DOMContentLoaded', e => {
+    const cs = document.getElementsByClassName('carousel');
+    if (cs.length) {
+      for (let el of cs) {
+        new Carousel(el);
+      }
+    }
+  });
+})(window);
---
+++ gulpfile.js
@@ -0,0 +1,42 @@
+/*
+  Author: Pontus Östlund <https://profiles.google.com/poppanator>
+*/
+// jshint esversion: 6, node: true
+'use strict';
+const
+  gulp    = require('gulp'),
+  sass    = require('gulp-sass'),
+  plumber = require('gulp-plumber'),
+  newer   = require('gulp-newer'),
+  babel   = require('gulp-babel');
+
+const doCompress = false;
+
+gulp.task('sass', () => {
+  const sassOpts = {};
+
+  if (doCompress) {
+    sassOpts.outputStyle = 'compressed';
+  }
+
+  return gulp.src('src/**/*.scss')
+    .pipe(plumber())
+    .pipe(newer('build'))
+    .pipe(sass(sassOpts).on('error', sass.logError))
+    .pipe(gulp.dest('build'));
+});
+
+gulp.task('js', [], () => {
+  return gulp.src('src/carousel.js')
+    .pipe(plumber())
+    .pipe(newer('build'))
+    .pipe(babel())
+    .pipe(gulp.dest('build'));
+});
+
+gulp.task('watch', [ 'default' ], () => {
+  gulp.watch('src/**/*.scss', [ 'sass' ]);
+  gulp.watch('src/carousel.js', [ 'js' ]);
+});
+
+gulp.task('default', [ 'sass', 'js' ]);
