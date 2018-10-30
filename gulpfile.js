9efe4682775693fe520bcfb354d9da3bce2c8fca
Pontus Östlund <ponost@roxen.com>
2018-10-30 16:14:37 +0100

    Updated Gulp file and all NPM dependencies

--- gulpfile.js
+++ gulpfile.js
@@ -1,80 +1,76 @@
+// @ts-check
 /*
   Author: Pontus Östlund <https://profiles.google.com/poppanator>
+
+  NOTE: You need to `npm i -g gulp-cli`, if not already done, for this to run.
 */
-// jshint esversion: 6, node: true
-'use strict';
 const
-  gulp       = require('gulp'),
-  sass       = require('gulp-sass'),
-  plumber    = require('gulp-plumber'),
-  newer      = require('gulp-newer'),
-  rename     = require('gulp-rename'),
-  uglify     = require('gulp-uglify'),
-  babel      = require('gulp-babel'),
+  gulp = require('gulp'),
+  sass = require('gulp-sass'),
+  plumber = require('gulp-plumber'),
+  newer = require('gulp-newer'),
+  rename = require('gulp-rename'),
+  uglify = require('gulp-uglify'),
+  babel = require('gulp-babel'),
   sourcemaps = require('gulp-sourcemaps');
 
 const doCompress = true;
 
-gulp.task('sass', () => {
+const sassFuncLow = (src, dest) => {
   const sassOpts = {};
 
   if (doCompress) {
     sassOpts.outputStyle = 'compressed';
   }
 
-  return gulp.src('src/**/*.scss')
-    .pipe(plumber())
-    .pipe(newer('build'))
-    .pipe(sass(sassOpts).on('error', sass.logError))
-    .pipe(rename({ suffix: '.min' }))
-    .pipe(gulp.dest('build'));
-});
+  return () => {
+    return gulp.src(src)
+      .pipe(plumber())
+      .pipe(newer('build'))
+      .pipe(sass(sassOpts).on('error', sass.logError))
+      .pipe(rename({ suffix: '.min' }))
+      .pipe(gulp.dest(dest));
+  };
+};
 
-gulp.task('js', [], () => {
-  return gulp.src('src/carousel.js')
-    .pipe(plumber())
-    .pipe(newer('build'))
-    .pipe(sourcemaps.init())
-      .pipe(babel({ presets: ['es2015'] }))
+const jsFuncLow = (src, dest) => {
+  return () => {
+    return gulp.src(src)
+      .pipe(plumber())
+      .pipe(newer('build'))
+      .pipe(sourcemaps.init())
+      .pipe(babel({ presets: ["env"] }))
       .pipe(uglify())
       .pipe(rename({ suffix: '.min' }))
-    .pipe(sourcemaps.write('.'))
-    .pipe(gulp.dest('build'));
-});
+      .pipe(sourcemaps.write('.'))
+      .pipe(gulp.dest(dest));
+  };
+};
 
-gulp.task('site-js', [], () => {
-  return gulp.src([ 'site/src/*.js' ])
-    .pipe(plumber())
-    .pipe(newer('site/build'))
-    .pipe(sourcemaps.init())
-      .pipe(babel({ presets: ['es2015'] }))
-      .pipe(uglify())
-      .pipe(rename({ suffix: '.min' }))
-    .pipe(sourcemaps.write('.'))
-    .pipe(gulp.dest('site/build'));
-});
+const watchFunc = () => {
+  gulp.watch('src/**/*.scss', gulp.task('sass'));
+  gulp.watch('src/carousel.js', gulp.task('js'));
+  gulp.watch('site/src/*.js', gulp.task('site-js'));
+  gulp.watch('site/src/*.scss', gulp.task('site-sass'));
+};
 
-gulp.task('site-sass', () => {
-  const sassOpts = {};
+const sassFunc = sassFuncLow('src/**/*.scss', 'build');
+const siteSassFunc = sassFuncLow('site/src/*.scss', 'site/build');
+const jsFunc = jsFuncLow('src/carousel.js', 'build');
+const siteJsFunc = jsFuncLow('site/src/*.js', 'site/build');
 
-  if (doCompress) {
-    sassOpts.outputStyle = 'compressed';
-  }
+gulp.task('sass', sassFunc);
+gulp.task('site-sass', siteSassFunc);
+gulp.task('js', jsFunc);
+gulp.task('site-js', siteJsFunc);
+gulp.task('watch', watchFunc);
 
-  return gulp.src('site/src/*.scss')
-    .pipe(plumber())
-    .pipe(newer('site/build'))
-    .pipe(sass(sassOpts).on('error', sass.logError))
-    .pipe(rename({ suffix: '.min' }))
-    .pipe(gulp.dest('site/build'));
-});
+const allFuncs = [
+  gulp.task('sass'),
+  gulp.task('site-sass'),
+  gulp.task('js'),
+  gulp.task('site-js')
+];
 
-
-gulp.task('watch', [ 'default' ], () => {
-  gulp.watch('src/**/*.scss', [ 'sass' ]);
-  gulp.watch('src/carousel.js', [ 'js' ]);
-  gulp.watch('site/src/*.js', [ 'site-js' ]);
-  gulp.watch('site/src/*.scss', [ 'site-sass' ]);
-});
-
-gulp.task('default', [ 'sass', 'js', 'site-sass', 'site-js' ]);
+gulp.task('watch', gulp.parallel(allFuncs.concat(gulp.task('watch'))));
+gulp.task('default', gulp.parallel(allFuncs));
